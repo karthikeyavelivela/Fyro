7278c4d83a70d155309f8988909445ca05991f8a
karthikeyeacoder <velivelakarthikeya@gmail.com>
2026-05-02 18:09:34 +0530

    fix: remove .next-verify cache from git and update gitignore

--- client/.next-verify/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"SehlnNA/bMJ7zx8CmNQ1ws77idjm6dFV1nunNSB9wVs=\"}"
--- client/.next-verify/static/chunks/3953.cd9e8be61b3c728a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3953],{63953:function(){}}]);
